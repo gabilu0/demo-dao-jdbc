a36a4d5eb7cf5d47c0ef9332d766ed0102109764
Gabriel Santos <gabilubastos@gmail.com>
2023-07-25 15:28:00 -0300

    Classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -8,6 +11,9 @@ public class Program {
 
         Department obj = new Department(1, "Books");
 
+        Seller seller = new Seller(21, "bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
