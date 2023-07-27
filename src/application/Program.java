6cd89592e809ae0af3a5efc0fbdbc2bbbb6e724a
Gabriel Santos <gabilubastos@gmail.com>
2023-07-27 15:57:30 -0300

    Implementação update

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,6 +35,11 @@ public class Program {
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
 
+        System.out.println("\n=== TESTE 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Marta East");
+        sellerDao.update(seller);
+        System.out.println("Updated complete");
 
     }
 }
